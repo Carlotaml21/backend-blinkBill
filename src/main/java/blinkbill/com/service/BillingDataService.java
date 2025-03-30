e69800c8b48771d56c33f87ed0155fd5272c20b3
Carlotaml21 <carlotaml21@gmail.com>
2025-03-30 16:55:39 +0200

    add method getAllBillingData (tdd:green)

--- src/main/java/blinkbill/com/service/BillingDataService.java
+++ src/main/java/blinkbill/com/service/BillingDataService.java
@@ -1,6 +1,7 @@
 package blinkbill.com.service;
 
 import blinkbill.com.BillingData;
+import blinkbill.com.repository.BillingDataRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,8 +9,17 @@ import java.util.List;
 @Service
 public class BillingDataService {
 
-    public List<BillingData> getAllBillingData(String filter){
-        return List.of();
+    private final BillingDataRepository billingDataRepository;
+
+    public BillingDataService(BillingDataRepository billingDataRepository) {
+        this.billingDataRepository = billingDataRepository;
+    }
+
+    public List<BillingData> getAllBillingData(String filter) {
+        if (filter == null || filter.isEmpty()) {
+            return billingDataRepository.findAll();
+        }
+        return billingDataRepository.findAll(filter);
     }
 
 }
