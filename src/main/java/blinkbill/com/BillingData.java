f14b03c6ad857d05bd5e94ce0ac5530e0e028c86
Carlotaml21 <carlotaml21@gmail.com>
2025-04-06 18:35:40 +0200

    add constructor, getters and setters  (tdd:green)

--- src/main/java/blinkbill/com/BillingData.java
+++ src/main/java/blinkbill/com/BillingData.java
@@ -1,4 +1,98 @@
 package blinkbill.com;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "COMPANY")
 public class BillingData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "TAX_ID", nullable = false, unique = true)
+    private String taxId;
+
+    @Column(name = "ADDRESS", nullable = false)
+    private String address;
+
+    @Column(name = "CITY", nullable = false)
+    private String city;
+
+    @Column(name = "PROVINCE", nullable = false)
+    private String province;
+
+    @Column(name = "POSTAL_CODE", nullable = false)
+    private Integer postalCode;
+
+    public BillingData() {
+    }
+
+    public BillingData(String name, String taxId, String address, String city, String province, Integer postalCode) {
+        this.name = name;
+        this.taxId = taxId;
+        this.address = address;
+        this.city = city;
+        this.province = province;
+        this.postalCode = postalCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTaxId() {
+        return taxId;
+    }
+
+    public void setTaxId(String taxId) {
+        this.taxId = taxId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public Integer getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(Integer postalCode) {
+        this.postalCode = postalCode;
+    }
 }
