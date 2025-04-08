590f6ad8eb7f630475cf5aa81c831d110123a689
Carlotaml21 <carlotaml21@gmail.com>
2025-04-08 17:46:50 +0200

    add @PostMapping method

--- src/main/java/blinkbill/com/controllers/BillingDataController.java
+++ src/main/java/blinkbill/com/controllers/BillingDataController.java
@@ -3,10 +3,9 @@ package blinkbill.com.controllers;
 import blinkbill.com.BillingData;
 import blinkbill.com.service.BillingDataService;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +24,12 @@ public class BillingDataController {
         List<BillingData> billingDataList = billingDataService.getAllBillingData(filter);
         return ResponseEntity.ok(billingDataList);
     }
+
+    @PostMapping()
+    public ResponseEntity<BillingData> postBillingData(@RequestBody BillingData newbillingData) {
+        BillingData savedData = billingDataService.saveBillingData(newBillingData);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedData);
+    }
+
 
 }
