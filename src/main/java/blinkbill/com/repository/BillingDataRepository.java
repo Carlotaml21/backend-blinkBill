ea4a6a4dc0f2e8d062c2b24ef5bc125bb35ee826
Carlotaml21 <carlotaml21@gmail.com>
2025-04-19 12:39:07 +0200

    TDD green, add method to save in repository

--- src/main/java/blinkbill/com/repository/BillingDataRepository.java
+++ src/main/java/blinkbill/com/repository/BillingDataRepository.java
@@ -2,8 +2,6 @@ package blinkbill.com.repository;
 
 import blinkbill.com.BillingData;
 import org.springframework.stereotype.Repository;
-
-import java.util.Collections;
 import java.util.List;
 
 @Repository
@@ -17,6 +15,10 @@ public class BillingDataRepository {
 
     public List<BillingData> findAll() {
         return repositoryJPA.findAll();
+    }
+
+    public BillingData save(BillingData billingData) {
+        return repositoryJPA.save(billingData);
     }
 
     public List<BillingData> findAll(String filter) {
