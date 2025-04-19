5217bab53d0306f77fcfda9d5f7076bddd579665
Carlotaml21 <carlotaml21@gmail.com>
2025-04-19 12:56:42 +0200

    TDD green, add method to update in service

--- src/main/java/blinkbill/com/service/BillingDataService.java
+++ src/main/java/blinkbill/com/service/BillingDataService.java
@@ -3,6 +3,7 @@ package blinkbill.com.service;
 import blinkbill.com.BillingData;
 import blinkbill.com.repository.BillingDataRepository;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -25,6 +26,21 @@ public class BillingDataService {
 
     public BillingData save(BillingData billingData) {
         return billingDataRepository.save(billingData);
+    }
+
+    public BillingData update(Long id, BillingData billingData) {
+        BillingData existing = billingDataRepository
+                .findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Company with ID " + id + " not found"));
+
+        existing.setName(billingData.getName());
+        existing.setTaxId(billingData.getTaxId());
+        existing.setAddress(billingData.getAddress());
+        existing.setCity(billingData.getCity());
+        existing.setProvince(billingData.getProvince());
+        existing.setPostalCode(billingData.getPostalCode());
+
+        return billingDataRepository.save(existing);
     }
 
 
